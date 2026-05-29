a0e812404dc6e74c948fc01b516ed5e9360a1f1e
gorkemcamusoglu00 <gorkem.camusoglu75@gmail.com>
2026-05-29 14:27:11 +0300

    exception is added for movietime app

---
+++ src/main/java/org/example/movietime/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.example.movietime.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
